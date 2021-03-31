3b4e0eb325fb71554bfd07a861a78b9da5abaf7a
gabrielyea <gabriel.gabrielyea@gmail.com>
2021-03-31 11:21:37 -0600

    Update MagicSquare.js

--- MagicSquare.js
+++ MagicSquare.js
@@ -2,13 +2,19 @@
 function formingMagicSquare(s) {
     
     let allPossibleSquares = [];
+    let diffList = [];
     
     for(let i = 0; i < 8; i++){
         allPossibleSquares.push(generateVariation(i))
     }
-    
+    console.log(s);
     console.log(allPossibleSquares);
     
+    allPossibleSquares.forEach(magicSquare => {
+        diffList.push(compareSquares(s, magicSquare))
+    })
+    console.log(diffList)
+    return Math.min(...diffList);
 }
 
 //uso este como seed para generar todos
@@ -17,6 +23,7 @@ function getMagicSquare(){
     return newMS;
 }
 
+//podria mejorar esto para no tener que darle vueltas a todos los squares cuando quiero una semilla > 1
 //genera variaciones a partir de la seed
 function generateVariation(num){
     
@@ -36,7 +43,7 @@ function generateVariation(num){
     
     const reverse = matrix => matrix.map(row => row.reverse())
     
-    
+    //probablemente hay una forma super obvia de hacer esto pero no se me ocurrio
     for(let i = 0; i < num; i++){
         if(i < 3){
              reverse(transpose(array));
@@ -50,4 +57,17 @@ function generateVariation(num){
     }
     
     return array;
+}
+
+//regresa la diferencia total que existe entre las dos matrices
+function compareSquares(original, magicSquare){
+    let dif = 0;
+    
+    for(let i = 0; i < original.length; i++){
+        for(let j = 0; j < original.length; j++){
+            dif += Math.abs(original[i][j] - magicSquare[i][j]);
+        }
+    }
+    
+    return dif
 }
